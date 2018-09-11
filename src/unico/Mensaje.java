bfdf2e1f7268d3f396d88fc39a393315401dfa3e
Aflosada <af.losada@uniandes.edu.co>
2018-09-11 16:04:00 -0500

    Commit para probar en el computador legit

--- src/unico/Mensaje.java
+++ src/unico/Mensaje.java
@@ -2,4 +2,31 @@ package unico;
 
 public class Mensaje {
 
+	private int mensaje;
+	
+	
+	public Mensaje(int pMen)
+	{
+		mensaje = pMen;
+	}
+	
+
+	/**
+	 * @return the mensaje
+	 */
+	public int getMensaje() {
+		return mensaje;
+	}
+
+	/**
+	 * @param mensaje the mensaje to set
+	 */
+	public void setMensaje(int mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
+	
+	
+	
 }
